c8a91dd97886046926850939f22167e9540ab752
fitue <linjiang1024@qq.com>
2023-12-02 15:08:50 +0800

    feat: 增加了对话功能

--- bot.py
+++ bot.py
@@ -1,16 +1,46 @@
-from fastapi_poe import PoeBot, run
-from fastapi_poe.types import PartialResponse, SettingsRequest, QueryRequest, SettingsResponse
-from fastapi_poe.client import stream_request
-from typing import AsyncIterable
+import asyncio
+from fastapi_poe.types import ProtocolMessage
+from fastapi_poe.client import get_bot_response
+import json
 
-class GPT35TurboBot(PoeBot):
-    async def get_response(self, query: QueryRequest) -> AsyncIterable[PartialResponse]:
-        async for msg in stream_request(query, "GPT-3.5-Turbo", api_key=""):
-            yield msg
 
-    async def get_settings(self, setting: SettingsRequest) -> SettingsResponse:
-        return SettingsResponse(
-            server_bot_dependencies={"GPT-3.5-Turbo": 1}
-        )
+class Conversation(object):
+    def __init__(self, api_key, bot_name, system_message):
+        self.api_key = api_key
+        self.messages = []
+        self.response = ""
+        self.bot_name = bot_name
+        if system_message == "":
+            pass
+        else:
+            self.add_message(system_message, "system")
+
+    def get_responses(self):
+        async def get_responses(api_key, messages):
+            response = ""
+            async for partial in get_bot_response(
+                messages=messages, bot_name="ChatGPT", api_key=api_key
+            ):
+                data = partial.text
+                response += data
+            print("")
+            return response
+
+        return get_responses(self.api_key, self.messages)
+
+    def add_message(self, message, role):
+        message = ProtocolMessage(role=role, content=message)
+        self.messages.append(message)
+
+    def chat(self, message):
+        self.add_message(message, "user")
+        self.response = asyncio.run(self.get_responses())
+        self.add_message(self.response, "bot")
+        return self.response
+
+
 if __name__ == "__main__":
-    run(GPT35TurboBot(), access_key="")
+    conversation = Conversation("", "ChatGPT", "")
+    while True:
+        message = input("You: ")
+        print(conversation.chat(message))
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi_poe.types import ProtocolMessage
 from fastapi_poe.client import get_bot_response
 import json
 
+
 # Create an asynchronous function to encapsulate the async for loop
 async def get_responses(api_key, messages):
     response = ""
@@ -16,10 +17,9 @@ async def get_responses(api_key, messages):
     return response
 
 
-
 # Replace <api_key> with your actual API key, ensuring it is a string.
-api_key = ""
-messages  = []
+api_key = "tR4M-mL3mr_dNsH6ik19iF7AbMt4YcupQTk3JhsowVU"
+messages = []
 # Run the event loop
 # For Python 3.7 and newer
 message = ProtocolMessage(role="user", content="你好")
@@ -32,7 +32,9 @@ messages.append(message)
 response = asyncio.run(get_responses(api_key, messages))
 response = ProtocolMessage(role="bot", content=response)
 messages.append(response)
-message = ProtocolMessage(role="user", content="碳是生物体（动物植物的组成物质）和矿物燃料（天然气，石油和煤）的主要组成部分。")
+message = ProtocolMessage(
+    role="user", content="碳是生物体（动物植物的组成物质）和矿物燃料（天然气，石油和煤）的主要组成部分。"
+)
 messages.append(message)
 asyncio.run(get_responses(api_key, messages))
 
